935f25b61e05435e55a5211b6d63368e29627694
David <barrard@gmail.com>
2017-02-26 16:34:00 -1000

    made view path more dynamic

--- modules/routes.js
+++ modules/routes.js
@@ -1,9 +1,16 @@
-var home = '/home/sailor/cordova/newMapApp/www/'
+var dir = __dirname
+dir=dir.split('/')
+dir.length= dir.length-1
+dir=dir.join('/')
+var home = dir+'/www/'
+
 module.exports = {
 
 	homeRoute:function(req, res, next){
+		console.log('homeroutes')
 		console.log(__dirname)
-			res.sendFile(home+'index.html')
+			// res.sendFile(__dirname+'/../index.html')
+			// res.sendFile(home+'index.html')
 	}
 
 
