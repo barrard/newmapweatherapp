a5e8935c463f584f91923f73ae19836a6c17fc66
David <barrard@gmail.com>
2017-07-16 22:46:20 -1000

    mesage

--- www/js/loginRegister/loginRegister.js
+++ www/js/loginRegister/loginRegister.js
@@ -1,20 +1,33 @@
+"use strict";
+
 var userLoginBtnClicked=function(){
 	console.log('handle the login')
 	var username = $('#userLoginUsername').val()
 	var password = $('#userLoginPassword').val()
-	var userData = {
+	var loginData = {
 			username:username,
 			password:password
 		}
-	helpers.verifyUserLoginData(userData)
-	// $.get('http://192.168.200.93:4444/userLogin', userData)
-	$.get('https://72.234.40.8/userLogin', userData)
+	helpers.verifyUserLoginData(loginData)
+	$.ajax({
+		type:'get',
+		url:'http://192.168.200.93:4444/userLogin',
+		data: loginData,
+		xhrFields: {
+		    withCredentials: true
+		},
+		crossDomain: true
+	})
+	// $.get('https://meetapp.us/userLogin', userData)
 		.done(function(data){
 			$('#loginResults').text('data')
 		})
 		.success(function(data){
 			console.log('succes?')
 			$('#loginResults').html(JSON.stringify(data))
+			helpers.handleLoginResponse(data)
+
+			console.log(userData)
 		})
 		.fail(function(jq, s, r){
 			console.log('error')
@@ -26,6 +39,10 @@ var userLoginBtnClicked=function(){
 
 
 	var helpers = {
+		handleLoginResponse:function handlerloginresponse(data){
+			console.log('handle resposne form login function handler with data response')
+			console.log(data)
+		},
 		verifyUserLoginData:function(userData){
 		console.log('verify')
 
